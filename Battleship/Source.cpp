224f58bee38eb5c26910b3dd15233ffd05fc7aad
LailaKristine <Laila.kristine@hotmail.no>
2019-11-12 13:20:39 +0100

    functions

---
+++ Battleship/Source.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+void MakeEmptyBaord(char board[][6]);
+int RandomRow();
+int RandomColumn();
+int WriteLetters();
+void MakeBoard(int);
+void PrintBoard();
+void printPlayerBoard();
+void shoot();
+
+void MakeEmptyBaord(char board[][6]) {
+
+
+}
+
+int RandomRow() {
+
+}
+
+int RandomColumn() {
+
+}
+int WriteLetters() {
+
+}
+int MakeBoard(int NumberOfShips) {
+
+}
+
+void PrintBoard() {
+
+}
+void printPlayerBoard() {
+
+}
+void shoot() {
+
+}
+int main() {
+
+
+	return 0;
+}
