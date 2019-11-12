d6bba48e6c0ec690c493788d3e8d79af2488f934
meisam-t <44691953+meisam-t@users.noreply.github.com>
2019-11-12 13:11:02 +0100

    declaration of functions

--- Battleship/Source.cpp
+++ Battleship/Source.cpp
@@ -1,2 +1,45 @@
-njkmjklnkl7
-njknhjkjn
+#include <iostream>
+
+using namespace std;
+void MakeEmptyBaord(char board[][6]);
+int RandomRow();
+int RandomColumn();
+int WriteLetters();
+void MakeBoard(int);
+void PrintBoard();
+void printPlayerBoard();
+void shoot();
+
+void MakeEmptyBaord(char board[][6]){
+
+
+}
+
+int RandomRow(){
+
+}
+
+int RandomColumn(){
+  
+}
+int WriteLetters(){
+  
+}
+int MakeBoard(int NumberOfShips){
+  
+}
+
+void PrintBoard(){
+  
+}
+void printPlayerBoard(){
+  
+}
+void shoot(){
+  
+}
+int main(){
+  
+  
+  return 0;
+}
